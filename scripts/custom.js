c497cfc6fabdecca4ece7831800f0778f4c0e2ec
Mat Seifried <Mat@MacbookPro.local>
2016-12-15 17:11:39 -0500

    Add - Check for JSON change
    Added functionality for checking JSON file for changes (not working yet)

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	// Reload page on data update
+	var previous = JSON.stringify($.getJSON("data.json"));
+
+	setInterval(function() {
+			current = JSON.stringify($.getJSON("data.json"))
+
+			if (previous !== current) {
+				console.log('JSON modified')
+			} else {
+				console.log("No JSON change")
+			}
+	}, 2000)
+
+	// On form submit send data to write.php for writing to server JSON file
 	$(document).on('submit', 'form', function() {
 		// Save form data
 		var name = $("#name").val()
